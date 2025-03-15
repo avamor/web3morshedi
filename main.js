30035892d3239469d9d706d7b3fa7a2bdbe249c0
ava morshedi <avamorshedi21478@gmail.com>
2025-03-15 10:59:01 -0700

    change code

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
-// console.log("oiuytrdfsd");
-// const wrapper = document.querySelector(".wrapper");
-// const dino = wrapper.querySelector(".dino");
-// const dinoImg = wrapper.querySelector(".dino img");
-// const road = wrapper.querySelector(".road img");
-// const cactus = wrapper.querySelector(".cactus");
-// const cactusImg = wrapper.querySelector(".cactus img");
+console.log("oiuytrdfsd");
+const wrapper = document.querySelector(".wrapper");
+const dino = document.getElementById("dino");
+const dinoImg = document.getElementById("dino-img");
+const road = document.getElementById("road");
+const cactus = wrapper.querySelector(".cactus");
+const cactus1 = wrapper.querySelector("cactus img");
 
-// const gameStart = false;
+let gameStart = false;
+let jump = true;
+function initGame() {
+  if (!gameStart) {
+    startGame();
+  }
+  gameStart = true;
+}
+function startGame() {
+  road.classList.add("road");
+  cactus.classList.add("cuctus");
+  cactusInterval = setInterval(() => {
+    cactus1.src = `image/${randomCactus()}.png`;
+  }, 1500);
+  dinoRun();
+}
+function dinoRun() {
+  runInterval = setInterval(() => {
+    dinoImg.src = "image/dinorun1.png";
+    setTimeout(() => {
+      dinoImg.src = "image/dinorun2.png";
+    }, 200);
+  }, 300);
+}
+function dinoJump() {
+  dino.classList.add("jump");
+  setTimeout(() => {
+    dino.classList.remove("jump");
+  }, 500);
+}
 
-// let initGame = () => {
-//   if (!gameStart) {
-//     startGame();
-//   }
-//   gameStart = true;
-// };
-// let startGame = () => {
-//   road.classList.add("road-active");
-//   cactus.classList.add("cuctus-active");
-//   cactusInterval = setInterval(() => {
-//     cactusImg.src = `image/${randomCactus()}.png`;
-//   }, 1500);
-// };
-// let randomCactus = () => {
-//   let cactusImgs = ["cactus1", "cactus2", "cactus3"];
+let randomCactus = () => {
+  let cactusImgs = ["cactus1", "cactus2", "cactus3"];
 
-//   let randomImg = cactusImgs[Math.floor(Math.random() * cactusImgs.length)];
-//   return randomImg;
-// };
-// document.addEventListener("keydown", (e) => {
-//   if (e.code === "Space") {
-//     initGame();
-//   }
-// });
+  let randomImg = cactusImgs[Math.floor(Math.random() * cactusImgs.length)];
+  return randomImg;
+};
+document.addEventListener("keyup", function (e) {
+  if (e.code === "Space") {
+    initGame();
+  }
+  if (e.code === "ArrowUp") {
+    if (jump) {
+      dinoJump();
+    }
+  }
+});
